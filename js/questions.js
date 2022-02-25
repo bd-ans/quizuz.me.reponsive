9434ce8284621a0a809c7111f4e20865d17a7c10
Elayt <elaytpatters@gmail.com>
2022-02-25 18:10:40 +0300

    main

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+     numb: 1,
+     question: "HTML so'zining keygaytmasi?",
+     answer:  "Hyper Text Markup Language",
+     options: [
+        "Hyper Text Processor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+     ]
+  },
+  {
+     numb: 2,
+     question: "CSS so'zining keygaytmasi?",
+     answer:  "Cascading Style Sheet",
+     options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computr Style Sheet",
+        "Cascading Style Sheet"
+     ]
+  },
+  {
+     numb: 3,
+     question: "PHP so'zining keygaytmasi?",
+     answer:  "Hypertext Processor",
+     options: [
+        "Hypertext Processor",
+        "Hypertext Programming",
+        "Hypertext Preprogramming",
+        "Hometext Preprocessor"
+     ]
+  },
+  {
+     numb: 4,
+     question: "SQL so'zining keygaytmasi?",
+     answer:  "Structured Query Language",
+     options: [
+        "Stylish Question language",
+        "Stylesheet Querry language",
+        "Statment Question Language",
+        "Structured Query Language"
+     ]
+  },
+  {
+     numb: 5,
+     question: "XML so'zining keygaytmasi?",
+     answer:  "eXtensible Markup language",
+     options: [
+        "eXtensible Markup language",
+        "eXecutable Multiple language",
+        "eXTra Multi-Program Language",
+        "examine Multiple Language"
+     ]
+  },
+];
